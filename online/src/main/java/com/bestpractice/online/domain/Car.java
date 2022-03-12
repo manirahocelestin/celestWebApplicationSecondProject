b49a79ed606f5934a7a6b64cda0e7006375334cf
celestin <celestinmani@gmail.com>
2022-03-12 11:54:28 -0700

    I have been added car class

--- online/src/main/java/com/bestpractice/online/domain/Car.java
+++ online/src/main/java/com/bestpractice/online/domain/Car.java
@@ -1,2 +1,18 @@
-package com.bestpractice.online.domain;public class Car {
+package com.bestpractice.online.domain;
+
+import javax.persistence.*;
+import javax.websocket.ClientEndpoint;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "bland")
+    private String bland;
+    @Column(name = "plack")
+    private String plack;
 }
